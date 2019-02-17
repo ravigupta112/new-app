b6b2936dca624546ff2c0355942eea1802e02e98
Ravi Gupta <ravigupta112>
2019-02-17 20:40:23 +0530

    Created new recipe model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import{Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes : Recipe[] = [
+    new Recipe("Reciepe name", "this is test description", "https://csharpcorner-mindcrackerinc.netdna-ssl.com/article/basic-architecture-of-angular-2-applications/Images/image001.jpg")
+  ];
   constructor() { }
 
   ngOnInit() {
